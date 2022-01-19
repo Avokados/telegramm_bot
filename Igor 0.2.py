90b563e803fb8cbf5917033c1789109e9f955c26
Nikita <avokados99@gmail.com>
2022-01-19 13:32:04 +0700

    Добавили план работ

--- Igor 0.2.py
+++ Igor 0.2.py
@@ -1,6 +1,7 @@
 import fileinput
 
 from aiogram.dispatcher import Dispatcher
+from aiogram.types.base import InputFile
 from aiogram.utils import executor
 from aiogram import Bot
 from BotConfig import BotToken as bt
@@ -14,14 +15,15 @@ dp = Dispatcher(bot)
 @dp.message_handler(content_types=["photo"])
 async def get_photo(message):
     file_info = await bot.get_file(message.photo[-1].file_id)
-    await message.photo[-1].download(file_info.file_path.split('photos/')[1])  # ++
+    a = message.photo[-1].download(file_info.file_path.split('photos/')[1])  # ++
+
     print(file_info)
     chat_id = message.chat.id
     text = "Фото сохранено на сервере"
 
     await bot.send_message(chat_id=chat_id, text=text)
-    img = cv2.imread('file_13.jpg')
-
+    # img = cv2.imread('file_13.jpg')
+    img = a
     # n_img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
     n_img = cv2.GaussianBlur(img, (5, 5), 3)
     n_img = cv2.cvtColor(n_img, cv2.COLOR_BGR2GRAY)
@@ -30,8 +32,9 @@ async def get_photo(message):
 
     kernel = np.ones((3, 3), np.uint8)
     n_img = cv2.dilate(n_img, kernel, iterations=1)
-    kart = '/file_13.jpg'
-    await bot.send_photo(chat_id=chat_id, photo=kart)
+    n_img = InputFile(n_img)
+    await bot.send_photo(chat_id=chat_id, photo=n_img)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- igor_0.3.py
+++ igor_0.3.py
@@ -1,14 +1,8 @@
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from aiogram import Bot
+import telebot
 from BotConfig import BotToken as bt
-import cv2
 
-bot = Bot(token=bt)
-dp = Dispatcher(bot)
-
-@dp.message_handler(content_types=["photo"])
-async def get_photo(message):
-    file_info = await bot.get_file(message.photo[-1].file_id)
-    await message.photo[-1].download()
-
+bot = telebot.TeleBot(bt)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет")
+bot.infinity_poling()
